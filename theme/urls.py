2fd4f482b87caaac7523b90d23f5fc54dcaa0f69
rwangoga <akandwanahojonan256@gmail.com>
2025-06-29 19:09:08 +0100

    Add templates and .gitignore

--- theme/urls.py
+++ theme/urls.py
@@ -2,10 +2,13 @@
 
 from django.urls import path, include
 from . import views
-from .views import home
+
 
 urlpatterns = [
       # Include URLs from the theme app
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),    # This will serve home.html at /
+    path('about/', views.about, name='about'),
+    path('music/', views.music, name='music'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
 ]
--- theme/views.py
+++ theme/views.py
@@ -1,7 +1,18 @@
+# theme/views.py
+
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'theme/about.html')
+
+def music(request):
+    return render(request, 'music.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
